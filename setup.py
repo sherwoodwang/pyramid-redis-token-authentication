2f7be36ab7ccf3c03189a6e2d6b2699ab5815b33
Sherwood Wang <sherwood@wang.onl>
2016-03-15 02:30:33 +0800

    add metadata

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='pyramid_redis_token_authentication',
-      version='0.1',
+      version='0.1.1',
       description='Authenication policy for Pyramid which stores counterfoils of tokens in Redis',
       classifiers=[
           "Programming Language :: Python",
@@ -13,6 +13,7 @@ setup(name='pyramid_redis_token_authentication',
       ],
       author='Sherwood Wang',
       author_email='sherwood@wang.onl',
+      url='https://github.com/sherwoodwang/pyramid-redis-token-authentication',
       packages=find_packages(),
       install_requires=[
           'pyramid',
